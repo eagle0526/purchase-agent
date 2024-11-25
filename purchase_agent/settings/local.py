7732a82dd3bc498971557ed6b3f8c490ab7615a3
Yee <a034506618@gmail.com>
2024-11-25 16:49:49 +0800

    exec python migrate and add postgre database

--- purchase_agent/settings/local.py
+++ purchase_agent/settings/local.py
@@ -4,10 +4,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'purchase_agent',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
